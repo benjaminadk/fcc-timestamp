0bc0c8470d374f5ebc7047a05b45f4a0378737e4
Glitch (stump-peacock) <none>
2018-06-08 04:03:11 +0000

    💐🌛 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,46 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/timestamp/:date_string", function (req, res) {
+  let dateRaw
+  if(req.params.date_string.length > 10) {
+    dateRaw = parseInt(req.params.date_string)
+  } else {
+    dateRaw = req.params.date_string
+  }
+    const date = new Date(dateRaw)
+    if(date.toUTCString() === 'Invalid Date') {
+      res.send({ error: 'Invalid Date' })
+    } else {
+      res.send({ unix: date.getTime(), utc: date.toUTCString() })
+    }
+});
+
+app.get("/api/timestamp/", function (req, res) {
+    const date = new Date()
+    res.send({ unix: date.getTime(), utc: date.toUTCString() })
+})
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
